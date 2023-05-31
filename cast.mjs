0a6757825ba96cca8def5863849db1559c13beda
Anton Kats <antonkats@gmail.com>
2023-05-31 11:26:28 -0300

    fix: cast not being able to handle non-filo objects

--- cast.mjs
+++ cast.mjs
@@ -2,8 +2,9 @@ import {FILO_CONSTITUENTS, FILO_PROTOTYPE, FILO_TYPE_ID} from "./constants.mjs";
 
 export function cast(what, toType, doThrow) {
   const searchFor = toType[FILO_PROTOTYPE][FILO_TYPE_ID]
+  if (!what) return null
 
-  if (what[FILO_CONSTITUENTS].includes(searchFor) || what[FILO_TYPE_ID] === searchFor) {
+  if (what[FILO_CONSTITUENTS]?.includes(searchFor) || what[FILO_TYPE_ID] === searchFor) {
     return what
   } else {
     if (doThrow) throw new Error('Failed to cast')
